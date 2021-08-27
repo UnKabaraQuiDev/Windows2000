2d31cd0c4f643f4e6b2a315c1c0af06f5885ab0f
Poucy113 <poucy113@school.lu>
2021-08-27 20:31:19 +0200

    commit 1

---
+++ src/org/lcdd/windows2k/Windows2KMain.java
@@ -0,0 +1,13 @@
+package org.lcdd.windows2k;
+
+import org.lcdd.windows2k.frame.Windows2KFrame;
+
+public class Windows2KMain {
+
+	private static Windows2KFrame frame;
+	
+	public static void main(String[] args) {
+		frame = new Windows2KFrame();
+	}
+
+}
---
+++ src/org/lcdd/windows2k/frame/Windows2KFrame.java
@@ -0,0 +1,12 @@
+package org.lcdd.windows2k.frame;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Windows2KFrame extends JFrame {
+
+	public Windows2KFrame() {
+		super("Windows 2000 Simulation");
+	}
+	
+}
